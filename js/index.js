b71218325226e7d7c77ff1c658aeecca52b9ee55
binyoyo <qqxubin963@gmail.com>
2014-03-11 15:50:50 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+
+$(function(){
+	var $navA = $(".nav a");
+	var navScroll=[];
+	$navA.each(function(){
+		var top=$($(this).attr("href")).offset().top;
+		navScroll.push(top);
+	})
+	$(window).scroll(function(){
+		var nowTop=$(window).scrollTop();
+		var length=navScroll.length;
+		var index;
+		if(nowTop+60 > navScroll[length-1]){
+                index = length;
+        }else{
+             	for(var i=0;i<length;i++){
+                    if(nowTop+60 <= navScroll[i]){
+                        index = i;
+                        break;
+                        }
+                    }
+        }
+        $navA.removeClass("active");
+        $navA.eq(index-1).addClass("active");
+	})
+	$navA.click(function(b){
+		if ($("html, body").is(":animated"))
+                return !1;
+
+		$navA.removeClass("active");
+		var d = $(this), e = $(d.attr("href"));
+		b.preventDefault();
+		d.addClass("active");
+		e.gettop(600);
+		
+		});
+    
+	$(window).on("resize", function() {
+            $(".nav:visible").length && $navA.filter(".active").triggerHandler("click")
+        })
+})
